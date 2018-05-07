847175b5dcce612fb59a0d6feba48c4903f7a13b
hainiaoyu <hli47@stevens.edu>
2018-05-06 21:02:10 -0700

    Add Intent Comment

--- app/src/main/java/com/example/yimingli/firstsample/MainActivity.java
+++ app/src/main/java/com/example/yimingli/firstsample/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view) {
+        /* Intent is used to communicate with other activities. */
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
